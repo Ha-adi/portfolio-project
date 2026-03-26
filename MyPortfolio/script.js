156fbf9aa99d6787c211668929146469945fcf79
Ha-adi <haadikv12@gmail.com>
2026-03-26 22:08:39 +0530

    Update script.js

--- MyPortfolio/script.js
+++ MyPortfolio/script.js
@@ -1,5 +1,5 @@
-const supabaseUrl = "https://pufvuhulkttkzkxrconb.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB1ZnZ1aHVsa3R0a3preHJjb25iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ0OTg1ODMsImV4cCI6MjA5MDA3NDU4M30.0l8UnwSKCfqe5NYo70TFcQqCC2o-BNBJga2zFS_CrG4";
+const supabaseUrl = "https://xipboskoiqsxrdclrnlt.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhpcGJvc2tvaXFzeHJkY2xybmx0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ1MzQ0OTMsImV4cCI6MjA5MDExMDQ5M30.DWmtoWaiusvvFiPWI9HxAFrvKl3srIgmZuN38airlE0";
 
 const client = supabase.createClient(supabaseUrl, supabaseKey);
 
